ccf35600905b5a645e201bc965af665529af9c5a
Vanessa <nessa1vane@icloud.com>
2024-06-03 23:32:10 -0300

    adicionando arquivos de classes e aprendendo POO com C#

---
+++ Banda.cs
@@ -0,0 +1,20 @@
+public class Banda {
+    private List<Album> albums = new List<Album>();
+
+    public Banda(string nome) {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+
+    public void AdicionarAlbum(Album album) { 
+        albums.Add(album);
+    }
+
+    public void ExibirDiscografia() {
+        Console.WriteLine($"Discografia da banda {Nome}");
+        foreach (Album album in albums) {
+            Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+        }
+    }
+}
